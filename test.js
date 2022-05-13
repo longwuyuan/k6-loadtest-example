843cf1d754debd905b86ea04f59c5dfd3cd2f97f
Long Wu Yuan <longwuyuan@gmail.com>
2022-05-14 02:34:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ import { sleep } from 'k6';
 export const options = {
   duration: '1m',
   vus: 50,
+  hosts: {
+    'k6-test.ingress-nginx.local' : '127.0.0.1',
+  },
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
@@ -11,6 +14,6 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('http://localhost');
+  const res = http.get('http://k6-test.ingress-nginx.local');
   sleep(1);
 }
